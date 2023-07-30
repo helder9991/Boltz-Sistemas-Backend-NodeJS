2547264ce3809226ace27b0b1bf93c6d7f0d7a40
Hélder Afonso <helderafonsoss@hotmail.com>
2023-07-29 23:28:05 -0300

    feature: Adicao do funcao de listar as instalacoes

--- src/modules/instalacao/repository/interface/IInstalacaoRepository.ts
+++ src/modules/instalacao/repository/interface/IInstalacaoRepository.ts
@@ -1,3 +1,4 @@
+import { type QntItensSalvos } from 'modules/fatura/repository/interfaces/IFaturaRepository'
 import type ICreateInstalacaoDTO from 'modules/instalacao/dtos/ICreateInstalacaoDTO'
 import type IFindInstalacaoDTO from 'modules/instalacao/dtos/IFindInstalacaoDTO'
 import type Instalacao from 'modules/instalacao/entities/Instalacao'
@@ -5,6 +6,7 @@ import type Instalacao from 'modules/instalacao/entities/Instalacao'
 interface IInstalacaoRepository {
   create: (data: ICreateInstalacaoDTO) => Promise<Instalacao>
   find: (data: IFindInstalacaoDTO) => Promise<Instalacao | null>
+  list: () => Promise<[Instalacao[], QntItensSalvos]>
 }
 
 export default IInstalacaoRepository
